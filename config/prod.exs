2a9a739e9b9646183ef2423c3a92f111774ba6ff
corynorris <corynorris@gmail.com>
2020-04-10 16:42:28 -0400

    Removed static manifest from config

--- config/prod.exs
+++ config/prod.exs
@@ -12,7 +12,7 @@ use Mix.Config
 config :sec_data, SecDataWeb.Endpoint,
   http: [:inet6, port: System.get_env("PORT") || 4000],
   url: [scheme: "https", host: System.get_env("SERVER_URL"), port: 443],
-  cache_static_manifest: "priv/static/cache_manifest.json",
+  # cache_static_manifest: "priv/static/cache_manifest.json",
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
 
